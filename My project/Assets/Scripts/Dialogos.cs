d52f6a1ee9b065b5fe97ed0e3a6d52492c9909f1
AbSi040 <sqj2025794@est.univalle.edu>
2025-10-27 00:01:24 -0400

    ijiji

---
+++ My project/Assets/Scripts/Dialogos.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TypewriterEffectDialogue : MonoBehaviour
+{
+    public float typingSpeed = 0.05f;
+    public Text textComponent;
+    private string fullText;
+
+    void Start()
+    {
+        fullText = textComponent.text;
+        textComponent.text = "Eres una estudiante de la carrera de administración de empresas y " +
+            "\r\nllevas de pareja con Mateo, un chico de la carrera de Medicina, \r\nbastante popular " +
+            "en su clase por ser atractivo. Llevas con él\r\npor tres meses, la relación va bien hasta que un día,\r\npasa algo en el receso...";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in fullText)
+        {
+            textComponent.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
+
---
+++ My project/Assets/Scripts/Registro.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RegistroManager : MonoBehaviour
+{
+    [Header("Campos de entrada")]
+    public InputField inputNombre;
+    public InputField inputCurso;
+    public InputField inputContrasena;
+
+    [Header("Botones")]
+    public Button btnRegistrarse;
+    public Button btnIniciarSesion;
+
+    private void Start()
+    {
+        btnRegistrarse.onClick.AddListener(Registrarse);
+        btnIniciarSesion.onClick.AddListener(AbrirInicioSesion);
+    }
+
+    void Registrarse()
+    {
+        string nombre = inputNombre.text;
+        string curso = inputCurso.text;
+        string contrasena = inputContrasena.text;
+
+        PlayerPrefs.SetString("registro_nombre", nombre);
+        PlayerPrefs.SetString("registro_curso", curso);
+        PlayerPrefs.SetString("registro_contrasena", contrasena);
+        PlayerPrefs.Save();
+
+        Debug.Log("Usuario registrado: " + nombre + " | Curso: " + curso);
+
+        SceneManager.LoadScene("InicioSesion");
+    }
+
+    void AbrirInicioSesion()
+    {
+        SceneManager.LoadScene("InicioSesion");
+    }
+}
